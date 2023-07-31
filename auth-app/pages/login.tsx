a055e3ef0d6306d1dc95c5ee841aff261127ed31
Fernando Rivera <fernando.rivbe@gmail.com>
2023-07-30 21:05:24 -0600

    Add app

---
+++ auth-app/pages/login.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./login.styles.css";
+import Image from 'next/image'
+
+
+const LoginScreen = () => {
+    return (
+        <div className="login-container">
+            <div className="login-box">
+                <Image
+                    src="/logo.svg"
+                    alt="logo"
+                    className="logo"
+                    width={100}
+                    height={24}
+                    priority
+                />
+                <h2>Welcome back!</h2>
+                <form>
+                    <input type="text" placeholder="email" />
+                    <input type="password" placeholder="password" />
+                    <a href="#" className="forgot-password">
+                        Forgot password
+                    </a>
+                    <button className="login-button">Login</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginScreen;
---
+++ auth-app/pages/signup.tsx
@@ -0,0 +1,113 @@
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import Image from "next/image";
+import "./signup.styles.css";
+
+interface ApiResponse {
+    error_code: string;
+    message: string;
+    target: string;
+    detail: {
+        [key: string]: string;
+    };
+}
+
+const SignUpScreen: React.FC = () => {
+    const [email, setEmail] = useState<string>("");
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    const handleSignUp = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const requestBody = {
+            email,
+            username,
+            password,
+        };
+
+        try {
+            const response = await fetch(
+                "https://z8g9btn11c.execute-api.us-east-1.amazonaws.com/v1/users",
+                {
+                    method: "POST",
+                    headers: {
+                        accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(requestBody),
+                }
+            );
+
+            const data: ApiResponse = await response.json();
+
+            if (!response.ok) {
+                // If there's an error response, set the error message
+                setErrorMessage(data.message);
+            } else {
+                // Handle successful signup here (e.g., redirect, show success message)
+                console.log("Signup successful!");
+            }
+        } catch (error) {
+            console.error("Error occurred:", error);
+            // Handle any other errors as needed
+        }
+    };
+
+    return (
+        <div className="signup-container">
+            <div className="signup-box">
+                <div className="logo-container">
+                    <Image
+                        src="/logo.svg"
+                        alt="logo"
+                        className="logo"
+                        width={100}
+                        height={24}
+                        priority
+                    />
+                </div>
+                <h2 className="signup-heading">Let's Get Started</h2>
+                <form className="signup-form" onSubmit={handleSignUp}>
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        required
+                        value={email}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setEmail(e.target.value)
+                        }
+                    />
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        required
+                        value={username}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setUsername(e.target.value)
+                        }
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        required
+                        value={password}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setPassword(e.target.value)
+                        }
+                    />
+                    <button type="submit" className="signup-button">
+                        Sign Up
+                    </button>
+                </form>
+                {errorMessage && (
+                    <div className="error-box" style={{ color: "red" }}>
+                        {errorMessage}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default SignUpScreen;
